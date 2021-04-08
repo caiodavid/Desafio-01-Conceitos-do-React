2d6b791ba9fb61859de40e618f33e4c97d415000
Caio David <caiodavid111@gmail.com>
2021-04-08 14:51:12 -0300

    Fix test

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -72,7 +72,7 @@ export function TaskList() {
         <div className="input-group">
           <input
             type="text"
-            placeholder="Adicionar novo To-Do"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
